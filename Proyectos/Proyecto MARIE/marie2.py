c21691568f44d8068731747dd4170481249d67cb
Natalia Palacios <natalia.palacios-r@mail.escuelaing.edu.co>
2019-05-17 15:58:47 -0500

    Proyecto MARIE finalizado

--- Proyectos/Proyecto MARIE/marie2.py
+++ Proyectos/Proyecto MARIE/marie2.py
@@ -8,21 +8,16 @@ def gcd(x,y):
     return x
   
 def main():
-    x,y,z = map(int, stdin.readline().split())
-    n = int(stdin.readline())
-    gcdd = gcd(x,y)
-    print(gcdd)
-    gcdd2 = gcd(gcdd,z)
-    print(gcdd2)
-    a = x/gcdd2
-    print(a)
-    b = y/gcdd2
-    print(b)
-    c = z/gcdd2
-    print(c)
-    suma = a + b + c
-    print(suma)
-    total = int(suma*n)
-    print(total)
+    while True:  
+        x,y,z = map(int, stdin.readline().split())
+        n = int(stdin.readline())
+        gcdd = gcd(x,y)
+        gcdd2 = gcd(gcdd,z)
+        a = x/gcdd2
+        b = y/gcdd2
+        c = z/gcdd2
+        suma = a + b + c
+        total = int(suma*n)
+        print(total)
 
 main()
